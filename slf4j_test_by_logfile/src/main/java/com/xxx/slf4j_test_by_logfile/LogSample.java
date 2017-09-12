4b16c6a9fbf18f365a75d6eb602d38fd13da5bf0
yuuyuu244 <yuuyuu244@yahoo.co.jp>
2017-09-12 23:46:33 +0900

    make classes...

--- slf4j_test_by_logfile/src/main/java/com/xxx/slf4j_test_by_logfile/LogSample.java
+++ slf4j_test_by_logfile/src/main/java/com/xxx/slf4j_test_by_logfile/LogSample.java
@@ -1,8 +1,38 @@
 package com.xxx.slf4j_test_by_logfile;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+
 public class LogSample {
+    public static final String logbackPath = "logback.xml";
+    /** getting logger. */
+    Logger logger = LoggerFactory.getLogger(LogSample.class);
+    
     public static void main(String... args) {
-        
+        // reloading logback configuration file.
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        JoranConfigurator configurator = new JoranConfigurator();
+        configurator.setContext(lc);
+        lc.reset();
+        try {
+            configurator.doConfigure(logbackPath);
+        } catch (JoranException ex) {
+            ex.printStackTrace();
+        }
+        LogSample _ = new LogSample();
+        _.outputLog("This message is for Test");
+    }
+    
+    /**
+     * this method is for Testing.
+     */
+    public void outputLog (String msg) {
+        logger.trace(msg);
+        logger.error(msg);
     }
 
 }
---
+++ slf4j_test_by_logfile/src/test/java/com/xxx/slf4j_test_by_logfile/LogSampleTest.java
@@ -0,0 +1,14 @@
+package com.xxx.slf4j_test_by_logfile;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LogSampleTest {
+
+    @Test
+    public void test() {
+        fail("まだ実装されていません");
+    }
+
+}
